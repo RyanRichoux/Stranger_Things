3a2775e40ef22360a89f24509aee0b889e33e93b
leslieguan <lguan@xula.edu>
2021-02-07 14:36:30 -0600

    react bootstrap

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Login } from './Login';
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import {
